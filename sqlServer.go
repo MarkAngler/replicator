80cee6b8e030a62963d0b43667f459e1604f20b7
Mark <mark.angler@outlook.com>
2023-10-22 15:48:51 -0400

    created basic sql server functions with gorm

---
+++ sqlServer.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"gorm.io/driver/sqlserver"
+	"gorm.io/gorm"
+)
+
+// ConnectToDatabase establishes a new database connection
+func connectToDatabase(serverName, port, username, password string) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("sqlserver://%s:%s@%s:%s?database=your_db", username, password, serverName, port)
+	db, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
+	return db, err
+}
+
+// InsertToTable inserts a new row into a table. Generic function, less type safe.
+func insertToTable(db *gorm.DB, tableName string, values interface{}) error {
+	result := db.Table(tableName).Create(values)
+	return result.Error
+}
