70f4d322b41be8ab20811785112961e30748a9bf
Mark <mark.angler@outlook.com>
2023-10-22 15:42:28 -0400

    moved configuration endpoints to separate file

--- main.go
+++ main.go
@@ -1,31 +1,8 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
-
-type sourceServer struct {
-	Server string `json:"server"`
-	Port   string `json:"port"`
-}
-
-var servers = []sourceServer{}
-
-func postSourceServers(c *gin.Context) {
-	var newServer sourceServer
-	if err := c.BindJSON(&newServer); err != nil {
-		return
-	}
-
-	servers = append(servers, newServer)
-	c.IndentedJSON(http.StatusCreated, newServer)
-}
-
-func getSourceServers(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, servers)
-}
 
 func main() {
 	r := gin.Default()
