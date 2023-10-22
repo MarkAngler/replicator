974263bd6bfab8ef4e2c26939b0de6e9d0b0ada2
Mark <mark.angler@outlook.com>
2023-10-22 17:09:50 -0400

    start front end

--- configEndPoints.go
+++ configEndPoints.go
@@ -7,11 +7,22 @@ import (
 )
 
 type sourceServer struct {
-	Server string `json:"server"`
-	Port   string `json:"port"`
+	Server   string `json:"server"`
+	Port     string `json:"port"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 var servers = []sourceServer{}
+
+// encrypt a string with a key
+func encrypt(key []byte, text string) string {
+	return text
+}
+
+func decrypt(key []byte, text string) string {
+	return text
+}
 
 func postSourceServers(c *gin.Context) {
 	var newServer sourceServer
