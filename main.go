c95d31b7ae5b4126adbf18f10bb151eaa149415d
Mark <mark.angler@outlook.com>
2023-10-28 20:26:22 -0400

    spacing

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var EncryptionKey string
+
 func main() {
+
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
 
@@ -14,5 +17,6 @@ func main() {
 
 	r.POST("/sourceServers", postSourceServers)
 	r.GET("/sourceServers", getSourceServers)
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
