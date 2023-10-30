3ebe5e477d0f3f59d3d9772b8f67a65a8c4a200f
Mark <mark.angler@outlook.com>
2023-10-29 21:17:39 -0400

    added new concept yaml types

--- configEndPoints.go
+++ configEndPoints.go
@@ -12,20 +12,59 @@ import (
 type sourceServer struct {
 	Server   string `json:"server"`
 	Port     string `json:"port"`
+	Type     string `json:"type"`
+	Database string `json:"database"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
-var servers = []sourceServer{}
+// Config is the top-level configuration structure.
+// Servers is a nested structure containing a map of server names to details.
+// ServerName is a map where the key is the server name and the value is the server details.
+// Config.Servers.ServerName["testservername"]
 
-func createYaml(y sourceServer, fileName string) ([]byte, error) {
+var Config struct {
+	Servers struct {
+		ServerName map[string]ServerDetails `yaml:"Servers"`
+	}
+}
+
+// ServerDetails holds all the configuration details for a single server.
+type ServerDetails struct {
+	Host string `yaml:"host"`
+
+	Port string `yaml:"port"`
+
+	Type string `yaml:"type"`
+
+	Database string `yaml:"database"`
+
+	Username string `yaml:"username"`
+
+	Password string `yaml:"password"`
+}
+
+func initializeYaml() {
+	allServerYaml, err := os.ReadFile("config.yaml")
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = yaml.Unmarshal(allServerYaml, &Config)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func addSourceServerToYaml(y sourceServer) ([]byte, error) {
+
 	data, err := yaml.Marshal(y)
+
 	if err != nil {
 		fmt.Println(err)
 		return nil, err
 
 	}
-	os.WriteFile(fileName, data, 0644)
+	os.WriteFile("config.yaml", data, 0644)
 	return data, nil
 }
 
@@ -43,11 +82,11 @@ func postSourceServers(c *gin.Context) {
 		return
 	}
 	newServer.Password = hashedPassword
-	//servers = append(servers, newServer)
-	createYaml(newServer, "config.yaml")
+
+	addSourceServerToYaml(newServer)
 	c.IndentedJSON(http.StatusCreated, newServer)
 }
 
-func getSourceServers(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, servers)
-}
+// func getSourceServers(c *gin.Context) {
+// 	c.IndentedJSON(http.StatusOK, servers)
+// }
